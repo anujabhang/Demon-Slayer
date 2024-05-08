e618febaf7b9b383ad33057fddf897b95784d602
anujabhang <abhang.apm@gmail.com>
2024-05-08 23:10:43 +0530

    features added

--- script.js
+++ script.js
@@ -10,6 +10,38 @@ document.onkeydown = (e)=>{
             tanji.classList.remove("animateTanjiro");
         }, 800);
     }
+    
+    else if(e.keyCode == 39){
+        tanji = document.querySelector(".tanjiro");
+        tx = parseInt(window.getComputedStyle(tanji, null).getPropertyValue("left"));
+        
+        tanji.style.left = tx + 112 + "px";
 
+    }
+    else if(e.keyCode == 37){
+        tanji = document.querySelector(".tanjiro");
+        tx = parseInt(window.getComputedStyle(tanji, null).getPropertyValue("left"));
+        
+        tanji.style.left = tx - 112 + "px";
 
+    }
+
+    
+    
 }
+
+setInterval(()=>{
+    gameOver = document.querySelector(".gameOver");
+    akaza = document.querySelector(".akaza");
+
+    tx = parseInt(window.getComputedStyle(tanji, null).getPropertyValue("left"));
+    ty = parseInt(window.getComputedStyle(tanji, null).getPropertyValue("bottom"));
+
+    ax = parseInt(window.getComputedStyle(akaza, null).getPropertyValue("left"));
+    ay = parseInt(window.getComputedStyle(akaza, null).getPropertyValue("top"));
+
+    offSetX = Math.abs(tx - ax);
+    offSetY = Math.abs(ty - ay);
+
+
+}, 100);
